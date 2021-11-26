44fa0ab864b46a39a3aff4cb4b5eecf8b696ffc0
memories2013 <nshivaprasad41@gmail.com>
2021-11-26 14:34:04 +0530

    Added pop and peek methods to empty the stack

--- src/Stack/StackTest.java
+++ src/Stack/StackTest.java
@@ -1,6 +1,6 @@
 package Stack;
 
-public class StackTest{
+public class StackTest {
 
 	public static void main(String[] args) {
 		
@@ -8,6 +8,10 @@ public class StackTest{
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
+		
+		stack.show();
+		System.out.println("");
+		stack.pop();
 		
 		stack.show();
 
--- src/Stack/Stacks.java
+++ src/Stack/Stacks.java
@@ -5,12 +5,37 @@ import java.util.LinkedList;
 public class Stacks {
 	
 	int top;
-	
+	public Stacks() {
+		this.top = -1;
+	}
+
 	LinkedList<Integer> list = new LinkedList<>();
 	
+	
 	public void push(int data) {
+		top++;
 		list.add(data);
 	}
+	
+	
+	// popping and peeking till the stack becomes empty
+	public void pop() {
+		if(list.isEmpty()) {
+			System.out.println("The Stack is empty");
+		}
+		else {
+			peek();
+			list.remove(top);
+			top--;
+			pop();
+		}
+	}
+	
+	
+	public void peek() {
+		System.out.println("Pop and Peek " +list.get(top));
+	}
+	
 	
 	public void show() {
 		for(int i=(list.size()-1); i>=0; i--){
